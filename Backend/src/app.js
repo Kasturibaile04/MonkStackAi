0d3217210ed375df19a19d6bf0402dae0b1cf03f
Kasturibaile04 <infokasturi23@gmail.com>
2026-08-01 06:55:02 +0530

    Update CORS origin to production frontend URL

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin:"monk-stack-ai-frontend.vercel.app",
     credentials: true
 }));
 
