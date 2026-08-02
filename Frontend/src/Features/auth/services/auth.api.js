f3a93d7ae3737e377890d4a39be5500145618a36
Kasturibaile04 <infokasturi23@gmail.com>
2026-08-02 16:55:14 +0530

    Update API base URL for authentication service

--- Frontend/src/Features/auth/services/auth.api.js
+++ Frontend/src/Features/auth/services/auth.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://monkstackai-1.onrender.com",
+    baseURL: "https://monkstackai-3.onrender.com",
     withCredentials: true
 });
 
